cfb7d9d300fc1f63c92682a7ab39ee2368a06b39
dnyanu <dnyaneshwarigojare@gmail.com>
2022-01-27 13:21:06 +0530

    Upto task 3 done

--- M1_Assistant.py
+++ M1_Assistant.py
@@ -3,10 +3,14 @@ from sys import maxunicode
 from threading import main_thread
 from tkinter import mainloop
 from unittest import expectedFailure
+import webbrowser
+from winreg import QueryInfoKey
 from pip import main
 import pyttsx3
 import datetime
-import speech_recognition as sr
+import speech_recognition as sr 
+import wikipedia
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -21,13 +25,13 @@ def speak(audio):
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
-        speak("Good Morning Sir!")
+        speak("Good Morning ma'am!")
 
     elif hour>=12 and hour<=18:
-        speak ("good afternoon sir!")
+        speak ("good afternoon ma'am!")
     
     else:
-        speak("Good Evening sir!")
+        speak("Good Evening sir !")
     
     speak("please tell me how may I help you ")
  
@@ -41,14 +45,37 @@ def takeCommand():
     try:
         print("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
-        print("User said: ",query)
+        print(f"User said: {query}\n")
 
     except Exception as e:
-        # prijnt(e)
-
+        # print(e)
         print("Say that again please....")
         return "None"
-        return query
+    return query
 if __name__ == "__main__":
     wishMe()
-    takeCommand()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching wikipedia...')
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")    
+
+
+
+
+     
+    
